96f6c66ae6c50250fea5c4a562c13c1cd39fc552
LambertZhaglog <2653697516@qq.com>
2019-12-04 12:01:42 +0800

    reaction complete, too

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -6,23 +6,47 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+  int co;//count the O atom number
+  int ch;//count the H atom number
+  struct lock lock;
+  struct condition ro;//to make water, an O atom ready
+  struct condition rh;//to make water, two H atoms ready
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+  reaction->co=0;
+  reaction->ch=0;
+  lock_init(&(reaction->lock));
+  cond_init(&(reaction->ro));
+  cond_init(&(reaction->rh));
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
-	// FILL ME IN
+  lock_acquire(&(reaction->lock));
+  reaction->ch+=1;
+  if(reaction->ch>1){
+    cond_signal(&(reaction->rh),&(reaction->lock));
+  }
+  cond_wait(&(reaction->ro),&(reaction->lock));
+  lock_release(&(reaction->lock));
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
-	// FILL ME IN
+  lock_acquire(&(reaction->lock));
+  reaction->co+=1;
+  while(reaction->ch<2){
+    cond_wait(&(reaction->rh),&(reaction->lock));
+  }
+  make_water();
+  cond_signal(&(reaction->ro),&(reaction->lock));
+  cond_signal(&(reaction->ro),&(reaction->lock));
+  reaction->co-=1;
+  reaction->ch-=2;
+  lock_release(&(reaction->lock));
 }
